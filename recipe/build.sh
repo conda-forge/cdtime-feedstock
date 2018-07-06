c82854cc643182fd2f1db010bd8f7071eae27228
Denis Nadeau <denis.nadeau@gmail.com>
2018-07-05 23:41:22 -0700

    remove LDSHARED for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,9 @@ export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC ${CFLAGS}"
 export CXXLAGS="${CFLAGS} ${CXXLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-export LDSHARED="$CC -shared -pthread"
 if [ `uname` == Linux ]; then
     LDSHARED="$CC -shared -pthread" ${PYTHON} setup.py install
 else
-    LDSHARED="$CC" ${PYTHON} setup.py install
+    ${PYTHON} setup.py install
 fi
 if [ `uname` == Darwin ]; then install_name_tool -change /System/Library/Frameworks/Python.framework/Versions/2.7/Python @rpath/libpython2.7.dylib ${SP_DIR}/pycf/*.so ; fi
