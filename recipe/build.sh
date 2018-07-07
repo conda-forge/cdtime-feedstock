0cc97b676795f8dafd5bc27ed5072da3ff85dd4a
Denis Nadeau <denis.nadeau@gmail.com>
2018-07-07 15:05:49 -0700

    fix LDSHARED env variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,10 @@ export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC ${CFLAGS}"
 export CXXLAGS="${CFLAGS} ${CXXLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
+
 if [ `uname` == Linux ]; then
-    LDSHARED="$CC -shared -pthread" ${PYTHON} setup.py install
+    export LDSHARED="$CC -shared -pthread" 
+    python setup.py install
 else
     ${PYTHON} setup.py install
 fi
